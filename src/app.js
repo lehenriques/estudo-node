0323fa761d5b78fe834d4a68b36abf82ae5b6e03
Leandro Henriques <lehenriques@gmail.com>
2020-07-01 15:15:19 -0300

    Configurando mongoose e banco de dados

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import routes from './routes';
 
 class App{
     constructor(){
         this.server = express();
+
+        mongoose.connect('mongodb+srv://aulanode:aulanode@devhouse-qibam.mongodb.net/aulanode?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
 
         this.middlewares();
         this.routes();
