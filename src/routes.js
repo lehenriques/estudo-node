7e293256e87a88fd7e419547305bf63889168b65
Leandro Henriques <lehenriques@gmail.com>
2020-07-01 21:06:13 -0300

    Manipulando parte de reservas

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import uploadConfig from './config/upload';
 import SessionController from './controllers/SessionController';
 import HouseController from './controllers/HouseController';
 import DashboardController from './controllers/DashboardController';
+import ReserveController from './controllers/ReserveController';
 
 const routes = new Router();
 const upload = multer(uploadConfig);
@@ -16,6 +17,10 @@ routes.post('/houses', upload.single('thumbnail'), HouseController.store);
 routes.put('/houses/:house_id', upload.single('thumbnail'), HouseController.update);
 routes.delete('/houses', HouseController.destroy);
 
-routes.get('/dashboard', DashboardController.show)
+routes.get('/dashboard', DashboardController.show);
+
+routes.post('/houses/:house_id/reserve', ReserveController.store);
+routes.get('/reserves', ReserveController.index);
+routes.delete('/reserves/cancel', ReserveController.destroy);
 
 export default routes;
